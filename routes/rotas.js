de40b436d143f658aff0a353c896bbe7d50af5fa
Richoland <ricardodallagnol.pf013@academico.ifsul.edu.br>
2024-11-06 03:03:43 -0300

    fix: conserta rotas

--- routes/rotas.js
+++ routes/rotas.js
@@ -1,16 +1,17 @@
 const { Router } = require('express');
-
 const seriesController = require('../controllers/seriesController');
 
 const rotas = new Router();
 
+// Rota para gerenciar todas as séries
 rotas.route('/series')
-     .get(seriesController.getSeries)
-     .post(seriesController.addSerie)
-     .put(seriesController.updateSerie)
+     .get(seriesController.getSeries)  // Pega todas as séries
+     .post(seriesController.addSerie); // Adiciona uma nova série
 
+// Rota para gerenciar uma série específica
 rotas.route('/series/:codigo')
-     .get(seriesController.getSeriePorCodigo)
-     .delete(seriesController.deleteSerie)
+     .get(seriesController.getSeriePorCodigo)  // Pega uma série por código
+     .put(seriesController.updateSerie)        // Atualiza uma série por código
+     .delete(seriesController.deleteSerie);    // Exclui uma série por código
 
 module.exports = rotas;
